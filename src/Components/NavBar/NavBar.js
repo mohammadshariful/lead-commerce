536d631e46d88fb5792fa05050abe09628244c13
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-04-03 02:27:58 +0600

    humbergur menubar added

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaBars, FaShoppingCart, FaTimes } from "react-icons/fa";
 import CustomLink from "../CustomLink/CustomLink";
 import "./NavBar.css";
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <nav className="nav-container">
       <h3 className="logo">
         <span>L</span>ead Commerce
       </h3>
-      <div className="nav-links">
+
+      <div className="menu-bar" onClick={() => setOpen(!open)}>
+        {open ? <FaTimes /> : <FaBars />}
+      </div>
+
+      <div className={`nav-links ${open ? "menuClose" : "menuOpen"}`}>
         <CustomLink to="/">DashBoard</CustomLink>
         <CustomLink to="/orders">Orders</CustomLink>
         <CustomLink to="/checkout">CheckOut</CustomLink>
         <CustomLink to="/payment">Payment</CustomLink>
         <CustomLink to="/address">Address</CustomLink>
+        <p className="shopping-cart">
+          <FaShoppingCart />
+          <span className="item">5</span>
+        </p>
       </div>
     </nav>
   );
